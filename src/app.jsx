68f96306dee0c70c838285e9b55926522131b544
秋秋 <qiu@qiuqiudeMacBook-Air.local>
2019-07-30 23:50:09 +0800

    feat initial build

---
+++ src/app.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import Schedule from './views/schedule';
+import './app.less';
+
+function App() {
+    return (
+        <Router>
+            <div className="app">
+                <div className="app-side">
+                    <ul>
+                        <li className="app-side-item">
+                            <Link to="/schedule">课程表</Link>
+                        </li>
+                        <li className="app-side-item">
+                            <Link to="/reminder">提醒列表</Link>
+                        </li>
+                        <li className="app-side-item">
+                            <Link to="/transfer">文件传输</Link>
+                        </li>
+                    </ul>
+                </div>
+
+                <div className="app-content">
+                    <Route path="/schedule" component={Schedule} />
+                </div>
+            </div>
+        </Router>
+    );
+}
+
+export default App;
